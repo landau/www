c2633a83e06b353c0727241e6cebc92f41073b5d
Trevor Landau <landautrevor@gmail.com>
2014-03-17 00:44:43 -0400

    fix bugs

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
-var cluster = require('cluster');
-var os = require('os');
+//var cluster = require('cluster');
+//var os = require('os');
 var app = require('./lib');
 
-var env = process.env;
+//var env = process.env;
 
+/*
 if (!cluster.isMaster) return;
 
 cluster.on('disconnect', function() {
@@ -15,5 +16,6 @@ cluster.on('disconnect', function() {
 os.cpus().forEach(function() {
   cluster.fork(env);
 });
+*/
 
 app.start();
