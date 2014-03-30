4b4c184811dfb3f9f2be7f43d37d61fdf3c30acd
Trevor Landau <landautrevor@gmail.com>
2014-03-30 16:06:55 -0400

    ready

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
 'use strict';
-//var cluster = require('cluster');
-//var os = require('os');
+
 var app = require('./lib');
-
-//var env = process.env;
-
-/*
-if (!cluster.isMaster) return;
-
-cluster.on('disconnect', function() {
-  cluster.fork(env);
-  console.error('Fork crashed! Booting new worker');
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('Listening on port %d', port);
 });
-
-os.cpus().forEach(function() {
-  cluster.fork(env);
-});
-*/
-
-app.start();
--- public/js/twitter/twitter-view.js
+++
@@ -1,9 +0,0 @@
-define(function () {
-    'use strict';
-
-    var Backbone = require('backbone');
-    var d3 = require('d3');
-
-    return Backbone.View.extend({
-    });
-});
